e7e2c5ea53ecbb34f5d50dfa393441be0dfd0a6a
Vitalii Solovei <vsolovei@smartling.com>
2022-01-21 15:36:03 +0200

    add elementor pro support tags and check (WP-681)

--- src/Bootloader.php
+++ src/Bootloader.php
@@ -8,6 +8,7 @@ use Smartling\Vendor\Symfony\Component\DependencyInjection\ContainerBuilder;
 class Bootloader {
     private const PLUGIN_NAME = 'Plugin Name';
     private const SUPPORTED_ELEMENTOR_VERSIONS = 'SupportedElementorVersions';
+    private const SUPPORTED_ELEMENTOR_PRO_VERSIONS = 'SupportedElementorProVersions';
     private const SUPPORTED_SMARTLING_CONNECTOR_VERSIONS = 'SupportedSmartlingConnectorVersions';
 
     private ContainerBuilder $di;
@@ -63,6 +64,16 @@ class Bootloader {
     {
         $allPlugins = get_plugins();
         $currentPluginName = self::getPluginName($pluginFile);
+        $errorMessage = self::checkSmartlingConnectorSupport($allPlugins, $pluginFile);
+        if ($errorMessage !== null) {
+            self::displayErrorMessage($errorMessage);
+            return;
+        }
+        $errorMessage = self::checkElementorSupport($allPlugins, $pluginFile);
+        if ($errorMessage !== null) {
+            self::displayErrorMessage($errorMessage);
+            return;
+        }
         foreach (
             [
                 'Elementor' => self::SUPPORTED_ELEMENTOR_VERSIONS,
@@ -83,6 +94,44 @@ class Bootloader {
         require_once __DIR__ . DIRECTORY_SEPARATOR . 'ElementorFieldsFilterHelper.php';
         require_once __DIR__ . DIRECTORY_SEPARATOR . 'ElementorAutoSetup.php';
         (new static($di))->run();
+    }
+
+    private static function checkSmartlingConnectorSupport(array $allPlugins, string $pluginFile): ?string
+    {
+        [$minVersion, $maxVersion] = explode('-', self::getPluginMeta($pluginFile, self::SUPPORTED_SMARTLING_CONNECTOR_VERSIONS));
+        $installed = self::findPluginByName($allPlugins, 'Smartling Connector');
+        if (!(!$installed || !self::versionInRange($installed['Version'] ?? '0', $minVersion, $maxVersion))) {
+            return "<strong>" . self::getPluginName($pluginFile) . "</strong> extension plugin requires <strong>Smartling Connector</strong> plugin version at least <strong>$minVersion</strong> and at most <strong>$maxVersion</strong>";
+        }
+        return null;
+    }
+
+    private static function checkElementorSupport(array $allPlugins, string $pluginFile): ?string
+    {
+        $installed = [];
+        $supported = [];
+        $supportedVersions = [
+            'Elementor' => explode('-', self::getPluginMeta($pluginFile, self::SUPPORTED_ELEMENTOR_VERSIONS)),
+            'Elementor Pro' => explode('-', self::getPluginMeta($pluginFile, self::SUPPORTED_ELEMENTOR_PRO_VERSIONS)),
+        ];
+        $elementor = self::findPluginByName($allPlugins, 'Elementor');
+        if ($elementor) {
+            $installed[] = 'Elementor';
+            if (self::versionInRange($elementor['Version'] ?? '0', $supportedVersions['Elementor'][0], $supportedVersions['Elementor'][1])) {
+                $supported[] = 'Elementor';
+            }
+        }
+        $elementorPro = self::findPluginByName($allPlugins, 'Elementor Pro');
+        if ($elementorPro) {
+            $installed[] = 'Elementor Pro';
+            if (self::versionInRange($elementorPro['Version'] ?? '0', $supportedVersions['Elementor Pro'][0], $supportedVersions['Elementor Pro'][1])) {
+                $supported[] = 'Elementor Pro';
+            }
+        }
+        if (count($installed) === 0 || count($installed) !== count($supported)) {
+            return "<strong>" . self::getPluginName($pluginFile) . "</strong> extension plugin requires <strong>Elementor</strong> plugin version at least <strong>{$supportedVersions['Elementor'][0]}</strong> and at most <strong>{$supportedVersions['Elementor'][1]}</strong> or <strong>Elementor Pro</strong> plugin version at least <strong>{$supportedVersions['Elementor Pro'][0]}</strong> and at most <strong>{$supportedVersions['Elementor Pro'][1]}</strong>";
+        }
+        return null;
     }
 
     /**
